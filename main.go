c9418f3a6d4f5f085e32e10713a7da8353739326
Dennis Kuhnert <dennis.kuhnert@sap.com>
2018-07-28 10:05:19 +0200

    2

--- main.go
+++ main.go
@@ -2,28 +2,39 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/rjz/githubhook"
-	"github.com/google/go-github/github"
+		"github.com/google/go-github/github"
 	"net/http"
 	"github.com/sirupsen/logrus"
-)
+	"io/ioutil"
+	)
 
 func main() {
 	http.HandleFunc("/", handler)
+
+	logrus.Infof("listening on 8080")
 	logrus.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	secret := []byte("don't tell!")
-	hook, err := githubhook.Parse(secret, r)
+	//secret := []byte("don't tell!")
+
+	logrus.Infof("header %v", r.Header)
+
+	//hook, err := githubhook.Parse(secret, r)
+	//if err != nil {
+	//	logrus.Errorf("failed to parse request: %v", err)
+	//	w.WriteHeader(500)
+	//	return
+	//}
+	payload, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		logrus.Errorf("failed to parse request: %v", err)
+		logrus.Errorf("failed to read request: %v", err)
 		w.WriteHeader(500)
 		return
 	}
 
 	var evt github.PullRequestEvent
-	if err := json.Unmarshal(hook.Payload, &evt); err != nil {
+	if err := json.Unmarshal(payload, &evt); err != nil {
 		logrus.Errorf("failed to unmarshal payload: %v", err)
 		w.WriteHeader(500)
 		return
