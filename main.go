19373edb4f1f219c55a3ae16ccc9d52d1bf2fca8
Dennis Kuhnert <dennis.kuhnert@sap.com>
2018-07-28 14:18:13 +0200

    3

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 		"github.com/google/go-github/github"
 	"net/http"
 	"github.com/sirupsen/logrus"
-	"io/ioutil"
-	)
+	"fmt"
+)
 
 func main() {
 	http.HandleFunc("/", handler)
@@ -20,26 +20,42 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	logrus.Infof("header %v", r.Header)
 
-	//hook, err := githubhook.Parse(secret, r)
-	//if err != nil {
-	//	logrus.Errorf("failed to parse request: %v", err)
-	//	w.WriteHeader(500)
-	//	return
-	//}
-	payload, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		logrus.Errorf("failed to read request: %v", err)
-		w.WriteHeader(500)
+	event := r.Header.Get("X-Github-Event")
+	if event == "" {
+		logrus.Errorf("failed to read event header")
+		w.WriteHeader(400)
 		return
 	}
+	switch event {
+	case "pull_request":
+		pullRequest(w, r)
+		return
+	default:
+		logrus.Errorf("unknown event: %s", event)
+	}
+	w.WriteHeader(400)
+}
 
+func pullRequest(w http.ResponseWriter, r *http.Request) {
 	var evt github.PullRequestEvent
-	if err := json.Unmarshal(payload, &evt); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&evt); err != nil {
 		logrus.Errorf("failed to unmarshal payload: %v", err)
 		w.WriteHeader(500)
 		return
 	}
+	if evt.PullRequest == nil || evt.PullRequest.Head == nil {
+		logrus.Errorf("missing data in pull request: %v", evt.PullRequest)
+		w.WriteHeader(500)
+		return
+	}
+	fmt.Println("evt.PullRequest.Head.Label", *evt.PullRequest.Head.Label)
+	fmt.Println("evt.PullRequest.Head.Ref", *evt.PullRequest.Head.Ref)
+	fmt.Println("evt.PullRequest.Head.SHA", *evt.PullRequest.Head.SHA)
+	fmt.Println("evt.Repo.Name", *evt.Repo.Name)
+	fmt.Println("evt.Repo.ID", *evt.Repo.ID)
+	fmt.Println("evt.Repo.CloneURL", *evt.Repo.CloneURL)
+	fmt.Println("evt.Repo.FullName", *evt.Repo.FullName)
+	fmt.Println("evt.Repo.GitURL", *evt.Repo.GitURL)
+	fmt.Println("evt.Repo.Language", *evt.Repo.Language)
 
-	w.WriteHeader(200)
-	logrus.Infof("succeeded", evt)
 }
