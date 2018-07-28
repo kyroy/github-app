7a059aaa721046899eebbd7fbc390abb5a53a4ae
Dennis Kuhnert <dennis.kuhnert@sap.com>
2018-07-28 22:27:00 +0200

    automate?

--- go.mod
+++ go.mod
@@ -1,16 +1,34 @@
 module github.com/kyroy/github-app
 
 require (
+	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
+	github.com/Microsoft/go-winio v0.4.9 // indirect
+	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
+	github.com/ahmetalpbalkan/dexec v0.0.0-20160630173852-2538e5e2198f
+	github.com/ahmetalpbalkan/go-dexec v0.0.0-20160630173852-2538e5e2198f // indirect
 	github.com/bradleyfalzon/ghinstallation v0.1.2
+	github.com/containerd/continuity v0.0.0-20180712174259-0377f7d76720 // indirect
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/dgrijalva/jwt-go v0.0.0-20180308231308-06ea1031745c // indirect
+	github.com/docker/docker v0.0.0-20180422163414-57142e89befe // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.3.3 // indirect
 	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/fsouza/go-dockerclient v1.2.2
+	github.com/gogo/protobuf v1.1.1 // indirect
 	github.com/golang/protobuf v1.1.0 // indirect
-	github.com/google/go-github v0.0.0-20180728155802-32c30013be02 // indirect
+	github.com/google/go-cmp v0.2.0 // indirect
+	github.com/google/go-github v0.0.0-20180728155802-32c30013be02
 	github.com/google/go-querystring v0.0.0-20170111101155-53e6ce116135 // indirect
+	github.com/gotestyourself/gotestyourself v1.4.0 // indirect
 	github.com/hpcloud/tail v1.0.0 // indirect
 	github.com/onsi/ginkgo v1.6.0 // indirect
 	github.com/onsi/gomega v1.4.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/opencontainers/runc v0.1.1 // indirect
+	github.com/opencontainers/selinux v1.0.0-rc1 // indirect
+	github.com/pkg/errors v0.8.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.0.6
 	github.com/stretchr/testify v1.2.2 // indirect
@@ -24,4 +42,5 @@ require (
 	gopkg.in/gemnasium/logrus-airbrake-hook.v2 v2.1.2 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.2.1 // indirect
+
 )
--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 	"context"
 	"github.com/bradleyfalzon/ghinstallation"
 	"io/ioutil"
+		"github.com/ahmetalpbalkan/dexec"
+	"github.com/fsouza/go-dockerclient"
+	"strings"
+	"bytes"
+	"time"
+	"regexp"
+	"strconv"
 )
 
 func test() {
@@ -31,10 +38,140 @@ func test() {
 	fmt.Println("aaa", a, b, c)
 }
 
-func main() {
-	test()
-	http.HandleFunc("/", handler)
+func testGoRepo(coURL, projPath, commit string) ([]*TestResult, error) {
+	//coURL := "https://github.com/Kyroy/testrepo.git"
+	//commit := "11621bbd1f7ef7ab05156563fc3ab9d663b8a0c"
+	//projPath := "github.com/Kyroy/testrepo"
+	commands := []string{"go version"}
+	commands = append(commands,
+		"go get -u golang.org/x/lint/golint",
+		fmt.Sprintf("mkdir -p $GOPATH/src/%s", projPath),
+		fmt.Sprintf("cd $GOPATH/src/%s", projPath),
+		fmt.Sprintf("git clone -q %s .", coURL),
+		fmt.Sprintf("git checkout -q %s", commit),
+		"echo '### go fmt'",
+		"go fmt ./...",
+		"echo '### golint'",
+		fmt.Sprintf(`golint $(go list ./...) | sed 's/'$(echo $GOPATH/src/%s/ | sed 's/\//\\\//g')'//g'`, projPath),
+		"echo '### go test'",
+		"go test -v ./...")
 
+	cl, err := docker.NewClient("unix:///var/run/docker.sock")
+	if err != nil {
+		logrus.Errorf("failed to create docker client: %v", err)
+		return nil, fmt.Errorf("failed to create docker client")
+	}
+	d := dexec.Docker{Client: cl}
+
+	m, err := dexec.ByCreatingContainer(docker.CreateContainerOptions{
+		Config: &docker.Config{Image: "golang:1.10"},
+	})
+	if err != nil {
+		logrus.Errorf("failed to dexec.ByCreatingContainer: %v", err)
+		return nil, fmt.Errorf("failed to dexec.ByCreatingContainer")
+	}
+
+	cmd := d.Command(m, "/bin/bash", "-c", strings.Join(commands, " && "))
+	var ba bytes.Buffer
+	cmd.Stderr = &ba
+	b, err := cmd.Output()
+	if err != nil {
+		msg := ba.String()
+		if msg != "" {
+			msg = " - " + msg
+		}
+		logrus.Errorf("command failed: %s%s", strings.TrimPrefix(err.Error(), "dexec: "), msg)
+	}
+	fmt.Printf("%s\n", b)
+
+	results := parseTestResults(projPath, b)
+	for _, res := range results {
+		fmt.Printf("- %v\n", res)
+	}
+	return results, nil
+}
+
+type TestResult struct {
+	File string
+	Line *int
+	Message string
+}
+
+func (r *TestResult) Valid() bool {
+	return r.File != "" && r.Line != nil && r.Message != ""
+}
+
+func NewTestResult(results [][]byte, names []string) (*TestResult, error) {
+	found := &TestResult{}
+	if len(results) > 0 {
+		for i, res := range results {
+			switch names[i] {
+			case "file":
+				found.File = string(res)
+			case "line":
+				l, _ := strconv.Atoi(string(res))
+				found.Line = &l
+			case "message":
+				found.Message = string(res)
+			}
+		}
+	}
+	if !found.Valid() {
+		return nil, fmt.Errorf("TestResult invalid: %v", found)
+	}
+	return found, nil
+}
+
+func parseTestResults(repo string, res []byte) []*TestResult {
+	var findings []*TestResult
+	lines := bytes.Split(res, []byte{'\n'})
+	goLintRe := regexp.MustCompile(`(?P<file>.+\.go):(?P<line>\d+):(?P<col>\d+): (?P<message>.+)`)
+	goLintNames := goLintRe.SubexpNames()
+	goTestRe := regexp.MustCompile(`\s*(?P<file>.+\.go):(?P<line>\d+): (?P<message>.+)`)
+	goTestNames := goTestRe.SubexpNames()
+	for i := 0; i < len(lines); i++ {
+		line := lines[i]
+		switch {
+		case bytes.HasPrefix(line, []byte("### golint")):
+			for i++; i < len(lines); i++ {
+				line = lines[i]
+				if bytes.HasPrefix(line, []byte("### ")) {
+					i--
+					break
+				}
+				results := goLintRe.FindSubmatch(line)
+				if res, err := NewTestResult(results, goLintNames); err == nil {
+					findings = append(findings, res)
+				}
+			}
+		case bytes.HasPrefix(line, []byte("### go test")):
+			for i++; i < len(lines); i++ {
+				line = lines[i]
+				if bytes.HasPrefix(line, []byte("### ")) {
+					i--
+					break
+				}
+				if bytes.HasPrefix(line, []byte("=== ")) || bytes.HasPrefix(line, []byte("--- ")) {
+					continue
+				}
+				results := goTestRe.FindSubmatch(line)
+				if res, err := NewTestResult(results, goTestNames); err == nil {
+					findings = append(findings, res)
+				}
+			}
+		}
+	}
+	return findings
+}
+
+func main() {
+	//setSuiteProgress("Kyroy", "testrepo", "Kyroy-patch-1", "11621bbd1f7ef7ab05156563fc3ab9d663b8a0c", 7719827)
+	//setRunCompleted("Kyroy", "testrepo", "Kyroy-patch-1", "11621bbd1f7ef7ab05156563fc3ab9d663b8a0c", 9669759)
+	//test()
+	//fmt.Printf("\n\n\n")
+	//testGoRepo()
+	http.HandleFunc("/", handler)
+	
 	logrus.Infof("listening on 8080")
 	logrus.Fatal(http.ListenAndServe(":8080", nil))
 }
@@ -51,11 +188,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	switch event {
-	case "pull_request":
-		pullRequest(w, r)
-		return
-	case "check_suite":
-		check_suite(w, r)
+	case "handleSuite":
+		handleSuite(w, r)
 		return
 	default:
 		logrus.Errorf("unknown event: %s", event)
@@ -63,39 +197,84 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(400)
 }
 
-func pullRequest(w http.ResponseWriter, r *http.Request) {
-	var evt github.PullRequestEvent
-	if err := json.NewDecoder(r.Body).Decode(&evt); err != nil {
-		logrus.Errorf("failed to unmarshal payload: %v", err)
-		w.WriteHeader(500)
-		return
-	}
-	if evt.PullRequest == nil || evt.PullRequest.Head == nil {
-		logrus.Errorf("missing data in pull request: %v", evt.PullRequest)
-		w.WriteHeader(500)
-		return
-	}
-	fmt.Println("evt.PullRequest.Head.Label", *evt.PullRequest.Head.Label)
-	fmt.Println("evt.PullRequest.Head.Ref", *evt.PullRequest.Head.Ref)
-	fmt.Println("evt.PullRequest.Head.SHA", *evt.PullRequest.Head.SHA)
-	if evt.Repo != nil {
-		fmt.Println("evt.Repo.Name", *evt.Repo.Name)
-		fmt.Println("evt.Repo.ID", *evt.Repo.ID)
-		fmt.Println("evt.Repo.CloneURL", *evt.Repo.CloneURL)
-		fmt.Println("evt.Repo.FullName", *evt.Repo.FullName)
-		fmt.Println("evt.Repo.GitURL", *evt.Repo.GitURL)
-	}
-	w.WriteHeader(200)
-}
-
-func check_suite(w http.ResponseWriter, r *http.Request) {
-	//body, err := ioutil.ReadAll(r.Body)
+func setSuiteProgress(client *github.Client, owner, repo, branch, sha string) (int64, error) {
+	//itr, err := ghinstallation.NewKeyFromFile(http.DefaultTransport, 15308, 262390, "kyroy-s-testapp.2018-07-28.private-key.pem")
 	//if err != nil {
-	//	logrus.Errorf("failed to read body: %v", err)
-	//	w.WriteHeader(500)
+	//	logrus.Errorf("failed to read key: %v", err)
 	//	return
 	//}
-	//fmt.Printf("check_suite: %s", body)
+	//
+	//// Use installation transport with client.
+	//client := github.NewClient(&http.Client{Transport: itr})
+	checkRun, response, err := client.Checks.CreateCheckRun(context.Background(), owner, repo, github.CreateCheckRunOptions{
+		Name: "first suite", // *
+		HeadBranch: branch, // *
+		HeadSHA: sha, // *
+		Status: github.String("in_progress"),
+	})
+	fmt.Println("setSuiteProgress", checkRun, response, err)
+	if err != nil {
+		return 0, fmt.Errorf("CreateCheckRun failed: %v", err)
+	}
+	if checkRun.ID == nil {
+		return 0, fmt.Errorf("checkRun.ID not set")
+	}
+	return checkRun.GetID(), nil
+}
+
+func setRunCompleted(client *github.Client, owner, repo, branch, sha string, checkRunID int64, results []*TestResult) {
+	//itr, err := ghinstallation.NewKeyFromFile(http.DefaultTransport, 15308, 262390, "kyroy-s-testapp.2018-07-28.private-key.pem")
+	//if err != nil {
+	//	logrus.Errorf("failed to read key: %v", err)
+	//	return
+	//}
+	//
+	//// Use installation transport with client.
+	//client := github.NewClient(&http.Client{Transport: itr})
+	if len(results) == 0 {
+		checkRun, response, err := client.Checks.UpdateCheckRun(context.Background(), owner, repo, checkRunID, github.UpdateCheckRunOptions{
+			CompletedAt: &github.Timestamp{Time: time.Now()},
+			Conclusion:  github.String("success"), // success, failure, neutral, cancelled, timed_out, or action_required
+		})
+		fmt.Println("[success] setSuiteProgress", checkRun, response, err)
+		return
+	}
+
+	var annotations []*github.CheckRunAnnotation
+	for _, res := range results {
+		annotations = append(annotations, &github.CheckRunAnnotation{
+			FileName: &res.File, // *
+			BlobHRef: github.String(fmt.Sprintf("https://github.com/%s/%s/blob/%s/%s", owner, repo, sha, res.File)), // *
+			StartLine: res.Line, // *
+			EndLine: res.Line, // *
+			WarningLevel: github.String("failure"),// * notice, warning, failure
+			Message: &res.Message, // *
+		})
+	}
+
+	checkRun, response, err := client.Checks.UpdateCheckRun(context.Background(), owner, repo, checkRunID, github.UpdateCheckRunOptions{
+		CompletedAt: &github.Timestamp{Time: time.Now()},
+		Conclusion: github.String("failure"), // success, failure, neutral, cancelled, timed_out, or action_required
+		Output: &github.CheckRunOutput{
+			Title: github.String("Test Output Title"), // *
+			Summary: github.String("test Output Summary"), // *
+			Annotations: annotations,
+			//Annotations: []*github.CheckRunAnnotation{
+			//	{
+			//		FileName:github.String("x_test.go"), // *
+			//		BlobHRef: github.String(fmt.Sprintf("https://github.com/%s/%s/blob/%s/%s", owner, repo, sha, "x_test.go")), // *
+			//		StartLine: github.Int(9), // *
+			//		EndLine: github.Int(9), // *
+			//		WarningLevel: github.String("warning"),// * notice, warning, failure
+			//		Message: github.String("don't use underscores in Go names; const x_y_x should be xYX"), // *
+			//	},
+			//},
+		},
+	})
+	fmt.Println("[failure] setSuiteProgress", checkRun, response, err)
+}
+
+func handleSuite(w http.ResponseWriter, r *http.Request) {
 	var evt github.CheckSuiteEvent
 	if err := json.NewDecoder(r.Body).Decode(&evt); err != nil {
 		logrus.Errorf("failed to unmarshal payload: %v", err)
@@ -135,23 +314,28 @@ func check_suite(w http.ResponseWriter, r *http.Request) {
 
 	switch status := evt.CheckSuite.GetStatus(); status {
 	case "queued":
-		checkRun, response, err := client.Checks.CreateCheckRun(context.Background(), evt.Repo.Owner.GetLogin(), evt.Repo.GetName(), github.CreateCheckRunOptions{
-			Name: "first suite", // *
-			HeadBranch: evt.CheckSuite.GetHeadBranch(), // *
-			HeadSHA: evt.CheckSuite.GetHeadSHA(), // *
-			Status: github.String("in_progress"),
-		})
-		fmt.Println("checkRun", checkRun)
-		fmt.Println("response", response)
-		fmt.Println("err", err)
+		runID, err := setSuiteProgress(client, evt.Repo.Owner.GetLogin(), evt.Repo.GetName(), evt.CheckSuite.GetHeadBranch(), evt.CheckSuite.GetHeadSHA())
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+
+		go func() {
+			results, err := testGoRepo(evt.Repo.GetCloneURL(), strings.TrimPrefix(evt.Repo.GetHTMLURL(), "https://"), evt.CheckSuite.GetHeadSHA())
+			if err != nil {
+				logrus.Errorf("testGoRepo failed: %v", err)
+				return
+			}
+			setRunCompleted(client, evt.Repo.Owner.GetLogin(), evt.Repo.GetName(), evt.CheckSuite.GetHeadBranch(), evt.CheckSuite.GetHeadSHA(), runID, results)
+		}()
+
+
 	//case "in_progress":
 	//case "completed":
 		// output
 	default:
 		fmt.Printf("unhandled check suite status: %s", status)
 	}
-
-
 
 	w.WriteHeader(200)
 }
